da1cea503b5840802669550882e928881a2ab4fc
Awais <64072435+CodeWithAwais@users.noreply.github.com>
2021-06-29 04:44:30 -0700

    Create random_fruits_game.py

---
+++ random_fruits_game.py
@@ -0,0 +1,32 @@
+import random
+
+
+amount = int(input("Enter the amount of money you wants to insert into the slow machin: "))
+fruits = ["Mangoes", "Banana", "Oranges", "Peach", "Grapes", "Apple", "Berries"]
+
+while True:
+    matched = []
+    for i in range(3):
+        random_fruit = random.choice(fruits)
+        print(random_fruit)
+
+        matched.append(random_fruit)
+    
+    unique_fruits = len(set(matched))
+    if unique_fruits == 3:
+        print("You won RS: 0")
+    if unique_fruits == 2:
+        print(f"You have won RS: {amount * 2}")
+    if unique_fruits == 1:
+        print(f"You have won RS: {amount * 3}")
+    else:
+        play_again = input("Do you want to play again [Yes/No]: ")
+        if play_again.lower() == 'yes':
+            continue
+
+        if play_again.lower() == 'no':
+            break
+        else:
+            print("You typed a wrong answer. Only choose between \"yes\" or \"No\"")
+            print("Restart the game... ")
+            break
